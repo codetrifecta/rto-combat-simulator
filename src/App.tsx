1fd6d46a208de0945055a41e4db6b8f2ee15b9f6
Muhammad Afuzarahman <theafuza@gmail.com>
2024-06-29 03:12:31 -0700

    Change grid hover background color based on player state

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,23 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import { useState } from "react";
-import { PlayerControlPanel } from "./ControlPanel";
+import { PlayerControlPanel } from "./PlayerControlPanel";
 import { Grid } from "./components/Grid";
 import { GRID_SIZE } from "./constants";
+import { PlayerState } from "./types";
 
 const gridDimensions = 6;
 const totalGridSize = gridDimensions * GRID_SIZE;
 
-const gridContainerClassName = `grid grid-rows-${gridDimensions} grid-cols-${gridDimensions}`;
+// const gridContainerClassName = `grid grid-rows-${gridDimensions} grid-cols-${gridDimensions}`;
 
 function App() {
   // const [count, setCount] = useState(0);
-  const [playerState, setPlayerState] = useState({
+  // const [playerHealth, setPlayerHealth] = useState(100);
+  const [playerState, setPlayerState] = useState<PlayerState>({
     isAttacking: false,
     isMoving: false,
     isUsingSkill: false,
-    isEndingTurn: false,
   });
 
   return (
@@ -28,11 +29,17 @@ function App() {
       </header>
       <div className="ml-auto mr-auto mb-10 ">
         <div
-          style={{ width: totalGridSize, height: totalGridSize }}
-          className={gridContainerClassName}
+          style={{
+            width: totalGridSize,
+            height: totalGridSize,
+            display: "grid",
+            gridTemplateColumns: `repeat(${gridDimensions}, ${GRID_SIZE}px)`,
+            gridTemplateRows: `repeat(${gridDimensions}, ${GRID_SIZE}px)`,
+          }}
+          // className={gridContainerClassName}
         >
           {Array.from({ length: 36 }).map((_, index) => (
-            <Grid key={index} />
+            <Grid key={index} playerState={playerState} />
           ))}
         </div>
       </div>
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,10 +1,18 @@
-// import { useState } from "react";
+import { FC } from "react";
 import { GRID_SIZE } from "../constants";
+import clsx from "clsx";
+import { PlayerState } from "../types";
 
-const className = `h-[${GRID_SIZE}px] w-[${GRID_SIZE}px] border-2 border-gray hover:border-black bg-white cursor-pointer `;
+const className = `border-2 border-gray hover:border-black bg-white cursor-pointer `;
 
-export const Grid = () => {
-  // const selected = useState(false);
-
-  return <div className={className}></div>;
+export const Grid: FC<{ playerState: PlayerState }> = ({ playerState }) => {
+  return (
+    <div
+      style={{ width: GRID_SIZE, height: GRID_SIZE }}
+      className={clsx(className, {
+        "hover:bg-red-500": playerState.isAttacking,
+        "hover:bg-blue-500": playerState.isMoving,
+      })}
+    ></div>
+  );
 };
