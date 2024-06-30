642b0d4faeff9183acf87e36eae5ac454d623d75
Muhammad Afuzarahman <theafuza@gmail.com>
2024-06-30 00:25:13 -0700

    Generate custom room

--- src/Room.tsx
+++ src/Room.tsx
@@ -3,11 +3,58 @@ import { Tile } from "./Tile";
 import { TILE_SIZE } from "./constants";
 import { PlayerState } from "./types";
 
+/**
+ * Room
+ * 0 - empty
+ * 1 - wall
+ * 2 - door
+ * 3 - player
+ * 4 - enemy
+ */
+
 const roomLength = 7;
 const totalRoomSize = roomLength * TILE_SIZE;
-const roomMatrix: number[][] = Array.from({ length: roomLength }, () => {
-  return Array.from({ length: roomLength }, () => 0);
-});
+
+// Initialize room matrix
+const roomMatrix: number[][] = Array.from({ length: roomLength }, () =>
+  Array.from({ length: roomLength }, () => 0)
+);
+
+console.log("pre", roomMatrix);
+
+// Generate room layout
+for (let row = 0; row < roomLength; row++) {
+  for (let col = 0; col < roomLength; col++) {
+    // Surround room with walls and place door in the middle of the top wall and bottom wall
+    if (
+      row === 0 ||
+      row === roomLength - 1 ||
+      col === 0 ||
+      col === roomLength - 1
+    ) {
+      if (col === Math.floor(roomLength / 2)) {
+        roomMatrix[row][col] = 2;
+      } else {
+        roomMatrix[row][col] = 1;
+      }
+
+      console.log("row", row);
+    }
+    // Place player in the bottom middle
+    else if (row === roomLength - 2 && col === Math.floor(roomLength / 2)) {
+      roomMatrix[row][col] = 3;
+    }
+    // Place enemy in the top middle
+    else if (row === 1 && col === Math.floor(roomLength / 2)) {
+      roomMatrix[row][col] = 4;
+    } else {
+      // Place walls everywhere else
+      roomMatrix[row][col] = 0;
+    }
+  }
+}
+
+console.log("post", roomMatrix);
 
 export const Room: FC<{ playerState: PlayerState }> = ({ playerState }) => {
   return (
@@ -19,16 +66,54 @@ export const Room: FC<{ playerState: PlayerState }> = ({ playerState }) => {
         gridTemplateColumns: `repeat(${roomLength}, ${TILE_SIZE}px)`,
         gridTemplateRows: `repeat(${roomLength}, ${TILE_SIZE}px)`,
       }}
-      // className={gridContainerClassName}
     >
       {roomMatrix.map((row, rowIndex) => {
         return row.map((tile, columnIndex) => {
-          return (
-            <Tile
-              key={rowIndex + "+" + columnIndex}
-              playerState={playerState}
-            />
-          );
+          // console.log(tile);
+          switch (tile) {
+            case 0:
+              return (
+                <Tile
+                  tileType={tile}
+                  key={`${rowIndex}-${columnIndex}`}
+                  playerState={playerState}
+                />
+              );
+            case 1:
+              return (
+                <Tile
+                  tileType={tile}
+                  key={`${rowIndex}-${columnIndex}`}
+                  playerState={playerState}
+                />
+              );
+            case 2:
+              return (
+                <Tile
+                  tileType={tile}
+                  key={`${rowIndex}-${columnIndex}`}
+                  playerState={playerState}
+                />
+              );
+            case 3:
+              return (
+                <Tile
+                  tileType={tile}
+                  key={`${rowIndex}-${columnIndex}`}
+                  playerState={playerState}
+                />
+              );
+            case 4:
+              return (
+                <Tile
+                  tileType={tile}
+                  key={`${rowIndex}-${columnIndex}`}
+                  playerState={playerState}
+                />
+              );
+            default:
+              return null;
+          }
         });
       })}
     </div>
--- src/Tile.tsx
+++ src/Tile.tsx
@@ -3,13 +3,31 @@ import { TILE_SIZE } from "./constants";
 import clsx from "clsx";
 import { PlayerState } from "./types";
 
-const className = `border-2 border-gray hover:border-black bg-white cursor-pointer `;
+const className = `border-2 border-gray hover:border-black cursor-pointer `;
 
-export const Tile: FC<{ playerState: PlayerState }> = ({ playerState }) => {
+/**
+ * Room
+ * 0 - empty
+ * 1 - wall
+ * 2 - door
+ * 3 - player
+ * 4 - enemy
+ */
+
+export const Tile: FC<{
+  tileType: number;
+  playerState: PlayerState;
+  classNames?: string;
+}> = ({ tileType, playerState, classNames = "" }) => {
   return (
     <div
       style={{ width: TILE_SIZE, height: TILE_SIZE }}
-      className={clsx(className, {
+      className={clsx(className, classNames, {
+        "bg-white": tileType === 0,
+        "bg-gray-500": tileType === 1,
+        "bg-yellow-500": tileType === 2,
+        "bg-green-500": tileType === 3,
+        "bg-red-500": tileType === 4,
         "hover:bg-red-500": playerState.isAttacking,
         "hover:bg-blue-500": playerState.isMoving,
       })}
