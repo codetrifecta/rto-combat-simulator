00608e983424eb68a62bf3198cfb223f1b12ad67
Muhammad Afuzarahman <theafuza@gmail.com>
2024-07-01 02:20:37 -0700

    Import GFS Neohellenic font and increase font sizes

--- src/PlayerInfo.tsx
+++ src/PlayerInfo.tsx
@@ -7,8 +7,8 @@ export const PlayerInfo: FC<{ playerState: PlayerState }> = ({
 }) => {
   return (
     <>
-      <h2 className="mb-2">Player</h2>
-      <div className="mb-10 w-[300px] ml-auto mr-auto text-left grid grid-rows-3 grid-cols-2 gap-1">
+      <h2 className="mb-2">Player Info</h2>
+      <div className="w-[300px] ml-auto mr-auto text-left grid grid-rows-2 grid-cols-2 gap-1">
         <p>Health: </p>
         <p>{playerState.health}</p>
         <p>Action Points: </p>
