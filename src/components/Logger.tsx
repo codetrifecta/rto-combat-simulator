c05e6a52066e78aa36bca59d6a453cd47cd9a07e
Muhammad Afuzarahman <theafuza@gmail.com>
2024-07-03 03:55:15 -0700

    Always scroll down to latest log

--- src/components/Logger.tsx
+++ src/components/Logger.tsx
@@ -1,9 +1,10 @@
-import { FC } from "react";
+import { FC, forwardRef } from "react";
 import { ROOM_LENGTH, TILE_SIZE } from "../constants";
 import { useLogStore } from "../store/log";
 import { ILog } from "../types";
+import clsx from "clsx";
 
-export const Logger = () => {
+export const Logger: FC = () => {
   const { logs } = useLogStore();
 
   return (
@@ -11,29 +12,36 @@ export const Logger = () => {
       className="bg-zinc-900 overflow-auto p-5 border-white border"
       style={{ maxHeight: ROOM_LENGTH * TILE_SIZE }}
     >
-      <h2 className="mb-5 pb-3 w-full border-b">Logger</h2>
-      {logs.map((log, index) => (
-        <LogItem key={index} log={log} />
-      ))}
+      <h2 className="mb-5 pb-3 w-full border-b">Game Log</h2>
+      {logs.map((log, index) => {
+        // Focus on the latest log
+
+        if (index === logs.length - 1) {
+          return (
+            <LogItem
+              key={index}
+              log={log}
+              ref={(ref) => {
+                if (ref) {
+                  ref.scrollIntoView();
+                }
+              }}
+            />
+          );
+        } else {
+          return <LogItem key={index} log={log} />;
+        }
+      })}
     </div>
   );
 };
 
-const LogItem: FC<{ log: ILog }> = ({ log }) => {
-  switch (log.type) {
-    case "info":
-      return (
-        <div className="flex mb-2 items-center rounded-md bg-neutral-800 px-2">
-          <p>{log.message}</p>
-        </div>
-      );
-    case "error":
-      return (
-        <div className="flex mb-2 items-center rounded-md bg-red-950 px-2">
-          <p>{log.message}</p>
-        </div>
-      );
-    default:
-      return <div />;
-  }
-};
+const LogItem = forwardRef<HTMLDivElement, { log: ILog }>(({ log }, ref) => {
+  const bgColor = log.type === "info" ? "bg-neutral-800" : "bg-red-950";
+
+  return (
+    <div className={clsx("mb-2 rounded-md py-1 px-3", bgColor)} ref={ref}>
+      <p className="text-left"> {log.message}</p>
+    </div>
+  );
+});
