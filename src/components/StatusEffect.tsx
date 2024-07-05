4c5988e37ed8e29bd1105b6d0bb7ef4a20542111
Muhammad Afuzarahman <theafuza@gmail.com>
2024-07-05 03:11:32 -0700

    Make Tooltip its own component

--- src/components/StatusEffect.tsx
+++ src/components/StatusEffect.tsx
@@ -1,6 +1,6 @@
-import { FC, useRef } from "react";
+import { FC } from "react";
 import { IStatus } from "../types";
-import clsx from "clsx";
+import { Tooltip } from "./Tooltip";
 
 export const StatusEffect: FC<{ status: IStatus }> = ({ status }) => {
   return (
@@ -14,35 +14,6 @@ export const StatusEffect: FC<{ status: IStatus }> = ({ status }) => {
         <p>{status.description}</p>
         <p className="">Lasts {status.duration} more turns.</p>
       </Tooltip>
-    </div>
-  );
-};
-
-const Tooltip: FC<{ children: JSX.Element[] }> = ({ children }) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const tooltipRect = ref.current?.getBoundingClientRect();
-
-  // Check if parent component is at the top half of the screen
-  let isTop = false;
-  const screenHeight = document.documentElement.clientHeight;
-  if (tooltipRect?.top && tooltipRect.top < screenHeight / 2) {
-    isTop = true;
-  }
-
-  console.log(isTop, tooltipRect?.top, screenHeight);
-
-  return (
-    <div
-      className={clsx(
-        "absolute inline-block opacity-0 group-hover:opacity-100 z-[-10] group-hover:z-50 bg-neutral-900 text-white p-2 rounded-lg shadow-lg transition-all ease duration-200 w-[250px]",
-        {
-          "bottom-[40px]": !isTop,
-          "top-[40px]": isTop,
-        }
-      )}
-      ref={ref}
-    >
-      {children}
     </div>
   );
 };
---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,34 @@
+import { FC, useRef } from "react";
+
+export const Tooltip: FC<{ children: JSX.Element[] }> = ({ children }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const tooltipRect = ref.current?.getBoundingClientRect();
+
+  // Check if parent component is at the top half of the screen
+  let isTop = false;
+  const screenHeight = document.documentElement.clientHeight;
+  if (tooltipRect?.top && tooltipRect.top < screenHeight / 2) {
+    isTop = true;
+  }
+
+  if (ref.current && tooltipRect) {
+    if (isTop) {
+      ref.current.style.bottom = `-${tooltipRect.height + 10}px`;
+    } else {
+      ref.current.style.top = `-${tooltipRect.height + 10}px`;
+    }
+  }
+
+  console.log(isTop, tooltipRect?.top, screenHeight, tooltipRect?.height);
+  //   isTop ? ` bottom-[-${tooltipRect?.height || "40px"}]` : "";
+  return (
+    <div
+      className={
+        "absolute inline-block opacity-0 group-hover:opacity-100 z-[-10] group-hover:z-50 bg-neutral-900 text-white p-2 rounded-lg shadow-lg transition-all ease duration-200 w-[250px]"
+      }
+      ref={ref}
+    >
+      {children}
+    </div>
+  );
+};
