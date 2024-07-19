9b50c335947839f2734d0d41c2f04b60655ca94b
Muhammad Afuzarahman <theafuza@gmail.com>
2024-07-19 02:40:02 -0700

    Setup prettier with eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
   overrides: [
     {
@@ -27,10 +28,18 @@ module.exports = {
     sourceType: 'module',
     project: './tsconfig.app.json',
   },
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
   rules: {
-    '@typescript-eslint/quotes': 0,
-    'linebreak-style': 0,
+    // '@typescript-eslint/quotes': 0,
+    // 'linebreak-style': 0,
+    'react/require-default-props': 0,
     'import/prefer-default-export': 0,
+    'react/react-in-jsx-scope': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
