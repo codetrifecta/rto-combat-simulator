692dccb0c98d56418b07d465b52005d4232fb28e
Muhammad Afuzarahman <theafuza@gmail.com>
2024-07-25 13:26:04 -0700

    Fix player control panel styling issue

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -181,7 +181,7 @@ export const Tile: FC<{
             ? SPRITE_ID.CELLAR_FLOOR_001
             : // Door IDs - Door background sprite is the same as wall sprite
               // Top door IDs
-              [365, 367].includes(tileID)
+              [365, 366, 367].includes(tileID)
               ? SPRITE_ID.CELLAR_WALL_005
               : // Bottom door IDs
                 [396, 398].includes(tileID)
