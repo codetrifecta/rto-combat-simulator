d596bc43c0d4017e42c9e1489c06398ceec0ce92
Muhammad Afuzarahman <theafuza@gmail.com>
2024-08-18 15:48:11 -0700

    Change TILE_TYPE enum to start from 0

--- src/constants/tile.ts
+++ src/constants/tile.ts
@@ -1,12 +1,12 @@
 import { SPRITE_ID } from './sprite';
 
-export const TILE_SIZE = 48; // Default to 50
+export const TILE_SIZE = 48;
 
 export enum TILE_TYPE {
-  NULL = 1,
-  FLOOR = 2,
-  WALL = 3,
-  DOOR = 4,
+  NULL = 0,
+  FLOOR = 1,
+  WALL = 2,
+  DOOR = 3,
 }
 
 interface ITile {
