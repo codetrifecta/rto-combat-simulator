54b1f8874dbf5d840615f9561af1f602816816c8
Muhammad Afuzarahman <theafuza@gmail.com>
2024-08-18 15:17:17 -0700

    Make healthbars not go below 0

--- src/components/Healthbar.tsx
+++ src/components/Healthbar.tsx
@@ -1,5 +1,5 @@
-import { FC } from "react";
-import { IEntity } from "../types";
+import { FC } from 'react';
+import { IEntity } from '../types';
 
 export const Healthbar: FC<{ entity: IEntity }> = ({ entity }) => {
   const { health, maxHealth } = entity;
@@ -11,10 +11,10 @@ export const Healthbar: FC<{ entity: IEntity }> = ({ entity }) => {
       <div className="relative w-full h-7 bg-gray-300 rounded-full flex justify-center items-center">
         <div
           className="absolute top-0 left-0 h-full bg-red-700 rounded-full transition-all ease-in-out duration-300 delay-0"
-          style={{ width: `${healthPercentage}%` }}
+          style={{ width: `${healthPercentage > 0 ? healthPercentage : 0}%` }}
         ></div>
         <span className="text-black z-10 font-bold cursor-default">
-          {health} / {maxHealth}
+          {health > 0 ? health : 0} / {maxHealth}
         </span>
       </div>
     </div>
