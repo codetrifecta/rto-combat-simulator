ca3e77cb4778edd3044b86e8237e30e66c4ec1de
Muhammad Afuzarahman <theafuza@gmail.com>
2024-08-25 03:34:00 -0700

    Add test case to check if room must have 4 unique rooms

--- src/utils/floor.test.ts
+++ src/utils/floor.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { floorToStringArray, generateFloorPlan } from './floor';
+import { floorToStringArray, generateFloorPlan, ROOM_TYPE } from './floor';
 
 describe('Initialize Floor Layout', () => {
   it('returns a 5x5 array layout of rooms', () => {
@@ -14,5 +14,33 @@ describe('Initialize Floor Layout', () => {
     for (let row = 0; row < floor.length; row++) {
       expect(floor[row].length).toBe(5);
     }
+  });
+
+  it('returns a 5x5 array layout of at least 1 common, 1 miniboss, 1 boss and 1 shop room', () => {
+    const floor = generateFloorPlan(true);
+
+    console.log(floorToStringArray(floor));
+
+    let hasCommon = false;
+    let hasMiniboss = false;
+    let hasBoss = false;
+    let hasShop = false;
+
+    // Expect the number of columns to be 5 each
+    for (let row = 0; row < floor.length; row++) {
+      for (let col = 0; col < floor[row].length; col++) {
+        if (floor[row][col] === ROOM_TYPE.COMMON) {
+          hasCommon = true;
+        } else if (floor[row][col] === ROOM_TYPE.MINIBOSS) {
+          hasMiniboss = true;
+        } else if (floor[row][col] === ROOM_TYPE.BOSS) {
+          hasBoss = true;
+        } else if (floor[row][col] === ROOM_TYPE.SHOP) {
+          hasShop = true;
+        }
+      }
+    }
+
+    expect(hasCommon && hasMiniboss && hasBoss && hasShop).toBe(true);
   });
 });
