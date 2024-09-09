b2fb3acca2787b758a16948ba1c2b0e8b16e15e4
Muhammad Afuzarahman <theafuza@gmail.com>
2024-09-09 00:11:49 -0700

    Implement open doors when room is over

---
+++ src/components/RoomDoorArt.tsx
@@ -0,0 +1,67 @@
+import { FC, useEffect, useRef } from 'react';
+
+import defaultRoom from '../assets/sprites/tiles/room_demo_door.png';
+import { TILE_SIZE } from '../constants/tile';
+import { useGameStateStore } from '../store/game';
+
+export const RoomDoorArt: FC<{
+  width: number;
+  height: number;
+  grayscale?: boolean;
+}> = ({ width, height, grayscale }) => {
+  const { isRoomOver, floorArtFile } = useGameStateStore();
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return;
+    }
+
+    const context = canvas.getContext('2d');
+    if (!context) {
+      return;
+    }
+
+    const image = new Image();
+
+    let imgSrc = floorArtFile;
+
+    if (!imgSrc) {
+      imgSrc = defaultRoom;
+    }
+
+    if (!isRoomOver) {
+      return;
+    } else {
+      imgSrc = defaultRoom;
+    }
+
+    image.src = imgSrc;
+
+    image.onload = function () {
+      if (!context) return;
+      context.reset();
+      context.imageSmoothingEnabled = false;
+
+      if (grayscale) {
+        context.filter = 'grayscale(1)';
+      }
+
+      const width = (image.naturalWidth / 16) * TILE_SIZE;
+      const height = (image.naturalHeight / 16) * TILE_SIZE;
+
+      context.drawImage(image, 0, 0, width, height);
+    };
+  }, [canvasRef.current, width, height, grayscale, isRoomOver, floorArtFile]);
+
+  return (
+    <canvas
+      className="relative pointer-events-none"
+      ref={canvasRef}
+      width={width}
+      height={height}
+    ></canvas>
+  );
+};
